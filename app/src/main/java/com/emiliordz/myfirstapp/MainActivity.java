b980ae172092b1fd9d6302f3fc4595fed8d4dd84
EmilRdz <42845858+EmilRdz@users.noreply.github.com>
2018-09-26 12:59:30 -0500

    ...

--- app/src/main/java/com/emiliordz/myfirstapp/MainActivity.java
+++ app/src/main/java/com/emiliordz/myfirstapp/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TextView tv = findViewById(R.string._416074642);
+        TextView tv = findViewById(R.string.num_cuenta);
         tv.setText("416074642");
     }
 }
