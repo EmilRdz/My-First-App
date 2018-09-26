baa01bc1f45f8babf2aede424491afbbb09cf27e
EmilRdz <42845858+EmilRdz@users.noreply.github.com>
2018-09-26 15:01:56 -0500

    ...

--- app/src/main/java/com/emiliordz/myfirstapp/MainActivity.java
+++ app/src/main/java/com/emiliordz/myfirstapp/MainActivity.java
@@ -2,7 +2,11 @@ package com.emiliordz.myfirstapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+
+import java.time.LocalTime;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,7 +14,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TextView tv = findViewById(R.string.num_cuenta);
-        tv.setText("416074642");
+        final TextView tv = findViewById(R.id.btn);
+        final Button btn = findViewById(R.id.btn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tv.setText(LocalTime.now().toString());
+            }
+        });
     }
 }
